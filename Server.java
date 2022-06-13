b21a7da494ddea61e945c4c48a0ead267b464ed5
alefmiguel <91097955+alefmiguel@users.noreply.github.com>
2022-06-13 18:34:15 -0300

    tratando comunicação e alterando protocolo porta 8000

--- Server.java
+++ Server.java
@@ -1,10 +1,11 @@
 // IMPORTAÇÃO DAS EXCEÇÕES E DO PACOTE SERVERSOCKET
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    // INICIALIZAÇÃO DA VARIÁVEL SOCKET DO SERVIDOR
     private ServerSocket serverSocket;
 
     // MÉTODOS GETTERS E SETTERS
@@ -17,7 +18,7 @@ public class Server {
     }
 
     // TRATAR O PROBLEMA NA CRIAÇÃO DA CONEXÃO
-    public void createServerSocket() throws IOException {
+    public void createServerSocket(int i) throws IOException {
         serverSocket = new ServerSocket(8000);
     }
 
@@ -26,16 +27,58 @@ public class Server {
         return socket;
     }
 
-    public void catchException (Socket socket) {
+    public void closeSocket(Socket s) throws IOException {
+        s.close(); // fechamento de um socket qualquer
+
+        // quando eu quiser desconectar um cliente do server
+        // terminar comunicação com o cliente
+        // conversa do protocolo etc (xauzinho)
+    }
+
+    public void catchConnection (Socket socket) throws IOException {
         // protocolo da aplicação (de cada uma)
-        
+        // enviar e receber mensagem por gosto
+        // criação de stream in e stream out
+
+        try {
+            // primeiro o output
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+            // depois o input
+            ObjectInputStream input = new ObjectInputStream (socket.getInputStream());
+
+            // protocolo simples (HELLO WORLD // HELLO WORLD)
+            String msg = input.readUTF();
+            System.out.println("Mensagem recebida: " + msg);
+            output.writeUTF("HELLO WORLD");
+
+            input.close();
+            output.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            // erro naquela comunicação em específico
+        }
+        finally { 
+            // tudo que abre fecha
+            // isso independete do erro ou não
+
+            closeSocket(socket);
+        }
     }
 
      public static void main(String[] args) {
         try {
             Server server = new Server();
-            server.createServerSocket();
-            Socket socket = server.waitConnection();
+            System.out.println("Aguardando conexão...");
+
+            server.createServerSocket(8000);
+            Socket socket = server.waitConnection(); // protocolo
+            System.out.println("Cliente conectado!");
+
+            // só vai conectar quando o cliente bater na porta
+            socket = server.waitConnection();
+            server.catchConnection(socket);
+            System.out.println("Cliente finalizado!");
         }
         catch (IOException e) {
             
